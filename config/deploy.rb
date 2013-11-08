af9f6958ea7bdb7b25abc4cb2b5e3ab0d4072966
Franco Catena <francocatena@gmail.com>
2013-11-08 18:17:07 -0300

    Add rbenv sets in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,11 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :deploy_via, :remote_cache
 set :scm, :git
 
+set :rbenv_type, :user
+set :rbenv_ruby, '2.0.0-p247'
+
+set :keep_releases, 5
+
 namespace :deploy do
 
   desc 'Restart application'
