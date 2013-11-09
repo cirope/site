12e3dff2edefb99230f622d4ebb5d3181371acc6
Franco Catena <francocatena@gmail.com>
2013-11-08 23:33:06 -0300

    Add upgrade nginx service to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,8 +18,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      run '/etc/init.d/unicorn upgrade'
     end
   end
 
