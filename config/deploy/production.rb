b02dd24c454864c8c8d0dce83c8c8c608ea4d75a
Franco Catena <francocatena@gmail.com>
2017-05-16 19:34:39 -0300

    New production site

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :stage, :production
 set :rails_env, 'production'
 
-role :app, %w{deployer@web.cirope.com}
-role :web, %w{deployer@web.cirope.com}
-role :db,  %w{deployer@web.cirope.com}
+role :app, %w{deployer@sites.cirope.com}
+role :web, %w{deployer@sites.cirope.com}
+role :db,  %w{deployer@sites.cirope.com}
 
-server 'web.cirope.com', user: 'deployer', roles: %w{web app db}
+server 'sites.cirope.com', user: 'deployer', roles: %w{web app db}
