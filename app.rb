c238a85d266f7e9a39380c70e40d0c9fbb12f381
Franco Catena <francocatena@gmail.com>
2019-10-07 18:58:49 -0300

    Cookie secret from ENV variable

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'cuba'
 require 'rack/protection'
 Cuba.use Rack::Session::Cookie,
   key: 'cirope.com',
-  secret: '04bd222a490a44b351e4bc9fc9ae0e2a572a03707a2760eaaf536aaba9cb15d74f60451e84d8b8ebf4fd138a6e363b1eedf2d5b56d430f5f570774da1c3feb8d'
+  secret: ENV['COOKIE_SECRET']
 Cuba.use Rack::Protection
 Cuba.use Rack::Protection::RemoteReferrer
 
